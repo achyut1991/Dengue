e3e599ddeb3c10897989f2620dcc2f011566ec4e
Achyut Balaji <achyut1991@gmail.com>
2012-05-21 23:51:37 +0800

    hopefully a safe commit

---
+++ xdengue/src/com/smartcommunities/xdengue/customLogoActivity.java
@@ -0,0 +1,24 @@
+package com.smartcommunities.xdengue;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class customLogoActivity extends Activity {
+ 
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+ 
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+ 
+        setContentView(R.layout.main);
+ 
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.logo);
+ 
+    }
+
+}
